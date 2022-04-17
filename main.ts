64412953f27febdcb8e65e7e19414601a1f09dc1
tommyzhang12345678 <102849446+tommyzhang12345678@users.noreply.github.com>
2022-04-17 12:42:28 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
-input.onButtonPressed(Button.A, function () {
-    robotbit.GeekServo(robotbit.Servos.S1, 0)
-})
-input.onButtonPressed(Button.B, function () {
-    robotbit.GeekServo(robotbit.Servos.S1, 90)
-})
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.A) || (input.soundLevel() >= 100 || false)) {
+        robotbit.MotorRun(robotbit.Motors.M1A, 100)
+        robotbit.GeekServo(robotbit.Servos.S1, 50)
+        basic.pause(5000)
+        basic.pause(5000)
+    } else {
+        robotbit.GeekServo(robotbit.Servos.S1, 0)
+        robotbit.MotorRun(robotbit.Motors.M1A, 0)
+    }
 })
